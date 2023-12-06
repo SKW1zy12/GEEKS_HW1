e12624a609a56e0c228b98ad6fa6ece3c49ca2e2
SKW1zy12 <lohmuhamatasiz@gmail.com>
2023-12-06 15:19:02 +0600

    hw

--- apps/settings/urls.py
+++ apps/settings/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from apps.settings.views import index
 
 urlpatterns = [
+     path('', index,name='index')
 
 ]
